1699d2742ac0e2937019f6f98784328c70c95d88
bookun <kutsuzawa2851@gmail.com>
2019-11-15 10:51:39 +0700

    add handler

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
 func main() {
-	println("hello")
+	http.HandleFunc("/", handler)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "hello cf")
 }
